ddc13b6a79feae97a1ebbf3af2457f5ad625e062
tbotalla <14.tomas@gmail.com>
2014-11-18 19:19:56 -0300

    Modificada linea que habia tocado de EdificadorTest

--- test/algo3/algocity/model/EdificadorTest.java
+++ test/algo3/algocity/model/EdificadorTest.java
@@ -71,7 +71,7 @@ public class EdificadorTest {
 		UnidadOcupable unaResidencia = e.construirUnidadResidencial();
 		UnidadOcupable otraResidencia = e.construirUnidadResidencial();
 		
-		assertNotEquals(unaResidencia, otraResidencia);
+		assertFalse(unaResidencia.equals(otraResidencia) );
 	}
 	
 	
