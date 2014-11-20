724cfbee8d7a8de30ee503e10ad2775ae3a7ba36
Carlos <carlosmbollero@gmail.com>
2014-11-20 16:51:57 -0300

    Modificados mapas, Mapa es abstracta ahora(puede haber cambios) y nueva clase MapaEdilicio

--- src/algo3/algocity/model/Mapa.java
+++ src/algo3/algocity/model/Mapa.java
@@ -1,6 +1,5 @@
 package algo3.algocity.model;
 
-import java.util.ArrayList;
 
 //import org.jgrapht.graph.DefaultEdge;
 //import org.jgrapht.graph.ListenableUndirectedGraph;
@@ -13,58 +12,19 @@ public abstract class Mapa {
 	public Mapa(int alto, int ancho) {
 		this.alto = alto;
 		this.ancho = ancho;
-
 	}
 
 	public abstract boolean agregar(Ubicable elemento, int i, int j);
+	public abstract void remover(int x, int y);
 
-	private boolean validarCoordenadas(int i, int j) {
-		if (this.estaVacio() || !this.estaDentroDeLimites(i, j)
-				|| this.tieneCoordenadaOcupada(i, j)) {
-			return false;
-		}
-
-		// for (Parcela p : this.lista){
-		// if (p.tieneCoordenadas(i, j)){
-		// return false;
-		// }
-		// }
-		return true;
-	}
-
-	private boolean estaDentroDeLimites(int i, int j) {
+	protected boolean estaDentroDeLimites(int i, int j) {
 		return ((i >= 0) && (i <= this.alto) && (j >= 0) && (j <= this.ancho));
 	}
 
-	public boolean tieneCoordenadaOcupada(int i, int j) {
-		if (this.estaVacio()) {
-			return false;
-		}
-		boolean resultado = false;
-		// if (!this.validarCoordenadas(i, j)){
-		// return false;
-		// }
-		this.i = this.lista.iterator();
-		while (!resultado && this.i.hasNext()) {
-			Parcela parcelaActual = this.i.next();
-			resultado = ((parcelaActual.tieneCoordenadas(i, j)) && (parcelaActual
-					.tieneContenido()));
-		}
+	public abstract boolean tieneCoordenadaOcupada(int i, int j);
 
-		return resultado;
-	}
+	public abstract boolean contiene(Ubicable unaUnidad);
 
-	public boolean contiene(Ubicable unaUnidad) {
-		this.i = this.lista.iterator();
-		boolean resultado = false;
-		while (!resultado && i.hasNext()) {
-			resultado = unaUnidad.equals(i.next().getContenido());
-		}
-		return resultado;
-	}
-
-	private boolean estaVacio() {
-		return (this.lista == null);
-	}
+	protected abstract boolean estaVacio();
 
 }
--- src/algo3/algocity/model/MapaEdilicio.java
+++ src/algo3/algocity/model/MapaEdilicio.java
@@ -1,9 +1,49 @@
 package algo3.algocity.model;
 
+import java.awt.Point;
+import java.util.HashMap;
+
 public class MapaEdilicio extends Mapa {
-	
-	public MapaEdilicio(int alto, int ancho){
+
+	HashMap<Point, Ubicable> posiciones;
+
+	public MapaEdilicio(int alto, int ancho) {
 		super(alto, ancho);
+		this.posiciones = new HashMap<Point, Ubicable>();
+	}
+
+	public boolean agregar(Ubicable elemento, int x, int y) {
+		if (!this.validarCoordenadas(x, y) || this.contiene(elemento)) {
+			return false;
+		}
+		Point clave = new Point(x, y);
+		if (!this.posiciones.containsKey(clave)) {
+			this.posiciones.put(clave, elemento);
+			return true;
+		}
+		return false;
+	}
+	
+	public void remover(int x, int y){
+		this.posiciones.remove(new Point(x, y));
+	}
+
+	private boolean validarCoordenadas(int x, int y) {
+		return (this.estaDentroDeLimites(x, y));
+	}
+
+	public boolean contiene(Ubicable unaUnidad) {
+		return (this.posiciones.containsValue(unaUnidad));
+	}
+
+	@Override
+	public boolean tieneCoordenadaOcupada(int x, int y) {
+		return (this.posiciones.containsKey(new Point(x, y)));
+	}
+
+	@Override
+	protected boolean estaVacio() {
+		return (this.posiciones.isEmpty());
 	}
 
 }
