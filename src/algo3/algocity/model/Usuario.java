ecbd559d8d702beb40ec0936b520d04a8740ac90
Carlos <carlosmbollero@gmail.com>
2014-11-22 20:15:40 -0300

    Limpieza de código

--- src/algo3/algocity/model/Usuario.java
+++ src/algo3/algocity/model/Usuario.java
@@ -1,11 +1,8 @@
 package algo3.algocity.model;
 
-import java.util.ArrayList;
-
 public class Usuario {
 
 	private String nombre;
-	ArrayList<Personaje> personajes;
 
 	public Usuario(String unNombre) {
 		this.nombre = unNombre;
