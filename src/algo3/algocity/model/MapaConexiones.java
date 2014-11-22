ee7fee956e5b911d50575fe762a21404dc0517f4
Carlos <carlosmbollero@gmail.com>
2014-11-21 23:23:10 -0300

    Agregado el mapa y sus tests.

--- src/algo3/algocity/model/MapaConexiones.java
+++ src/algo3/algocity/model/MapaConexiones.java
@@ -9,53 +9,71 @@
 package algo3.algocity.model;
 
 import java.awt.Point;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
 
+import org.jgrapht.alg.ConnectivityInspector;
 import org.jgrapht.graph.DefaultEdge;
-import org.jgrapht.graph.ListenableUndirectedGraph;
+import org.jgrapht.graph.SimpleGraph;
 
 public class MapaConexiones {
 
 	int alto;
 	int ancho;
-	HashMap<Point, Conector> lista;
-	ListenableUndirectedGraph<Conector, DefaultEdge> grafo;
+	LinkedHashMap<Point, Conector> lista;
+	SimpleGraph<Conector, DefaultEdge> grafo;
+	ConnectivityInspector<Conector, DefaultEdge> camino;
 
 	public MapaConexiones(int alto, int ancho) {
-
 		this.alto = alto;
 		this.ancho = ancho;
-		this.lista = new HashMap<Point, Conector>();
-		this.grafo = new ListenableUndirectedGraph<Conector, DefaultEdge>(
-				DefaultEdge.class);
+		this.lista = new LinkedHashMap<Point, Conector>();
+		this.grafo = new SimpleGraph<Conector, DefaultEdge>(DefaultEdge.class);
 	}
 
-	// TODO REVISAR
 	public boolean agregar(Conector elemento, int x, int y) {
-		if (this.validarCoordenadas(x, y) && !this.contiene(elemento) && !this.tieneCoordenadaOcupada(x, y)) {
+		if (this.validarCoordenadas(x, y) && !this.contiene(elemento)
+				&& !this.tieneCoordenadaOcupada(x, y)) {
 			this.lista.put(new Point(x, y), elemento);
 			this.grafo.addVertex(elemento);
-			this.actualizarGrafo(elemento, x,y);
+			this.actualizarGrafo(elemento, x, y);
+			return true;
+		}
+		return false;
+	}
+	
+	public int getCantidadElementos(){
+		return (grafo.vertexSet().size());
+	}
+
+	private void actualizarGrafo(Conector elemento, int x, int y) {
+		for (Entry<Point, Conector> entry : lista.entrySet()) {
+			if (hayDistanciaMinima(new Point(x, y), entry.getKey())) {
+				grafo.addEdge(elemento, entry.getValue());
+			}
+		}
+	}
+
+	private boolean hayDistanciaMinima(Point point, Point key) {
+		int x1 = (int) point.getX();
+		int y1 = (int) point.getY();
+		int x2 = (int) key.getX();
+		int y2 = (int) key.getY();
+		if ((Math.abs(x1 - x2) == 1) && (y1 == y2)){
+			return true;
+		}
+		if ((x1 == x2) && (Math.abs(y1 - y2) == 1)){
+			return true;
+		}
+		if ((Math.abs(x1 - x2)  == 1) && (Math.abs(y1 - y2) == 1)){
 			return true;
 		}
 		return false;
 	}
 
-	private void actualizarGrafo(Conector elemento, int x, int y) {
-		Map<Point, Conector> mapa = this.lista;
-		for (Map.Entry<Point, Conector> entry : mapa.entrySet()){
-			if (entry.getX() == (x + 1) || entry.getX() == (x - 1)){
-				if (entry.getY() == (y + 1) || entry.getY() == (y - 1)){
-					this.grafo.addEdge(elemento, this.lista.get(entry));
-				}
-			}
-		}
-		
-	}
-
 	public boolean contiene(Conector elemento) {
-		return (this.lista.containsValue(elemento) && this.grafo.containsVertex(elemento));
+		return (this.lista.containsValue(elemento) && this.grafo
+				.containsVertex(elemento));
 	}
 
 	private boolean validarCoordenadas(int x, int y) {
@@ -65,9 +83,23 @@ public class MapaConexiones {
 	private boolean estaDentroDeLimites(int x, int y) {
 		return ((x >= 0) && (x <= this.alto) && (y >= 0) && (y <= this.ancho));
 	}
-	
+
 	public boolean tieneCoordenadaOcupada(int x, int y) {
 		return (this.lista.containsKey(new Point(x, y)));
+	}
+
+	public boolean hayConexion(Point unPunto, Point otroPunto) {
+		this.camino = new ConnectivityInspector<Conector, DefaultEdge>(grafo);
+		return (camino.pathExists(lista.get(unPunto), lista.get(otroPunto)));
+	}
+	
+	public Point getCoordenadas(Conector elemento){
+		for (Entry<Point, Conector> entry : lista.entrySet()){
+			if (entry.getValue().equals(elemento)){
+				return entry.getKey();
+			}
+		}
+		return null;
 	}
 
 }
