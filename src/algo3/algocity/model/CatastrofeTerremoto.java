97c1ff6a4b96776057a24777f6ad6425a579149a
Carlos <carlosmbollero@gmail.com>
2014-11-26 19:34:48 -0300

    Cambio de nombre Terremoto --> CatastrofeTerremoto, para aportar mayor claridad

--- src/algo3/algocity/model/CatastrofeTerremoto.java
+++ src/algo3/algocity/model/CatastrofeTerremoto.java
@@ -1,11 +1,11 @@
 package algo3.algocity.model;
 
-public class Terremoto extends Catastrofe {
+public class CatastrofeTerremoto extends Catastrofe {
 	
 	int radio;
 	double tasa;
 	
-	public Terremoto(){
+	public CatastrofeTerremoto(){
 		radio = 25;
 		tasa = 1.5;
 	}
