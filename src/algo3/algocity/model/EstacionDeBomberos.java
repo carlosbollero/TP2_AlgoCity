2a457070883eb32ce1ef5c35abce307583082574
Carlos <carlosmbollero@gmail.com>
2014-11-27 16:07:43 -0300

    Implementado Visitor en EstacionDeBomberos

--- src/algo3/algocity/model/EstacionDeBomberos.java
+++ src/algo3/algocity/model/EstacionDeBomberos.java
@@ -1,21 +1,60 @@
 package algo3.algocity.model;
 
-public class EstacionDeBomberos extends Unidad {
+import java.util.ArrayList;
+
+public class EstacionDeBomberos extends Unidad implements Visitante {
 
 	private Conector conexion;
 
 	public EstacionDeBomberos() {
-		this.costo = 1500;
-		this.conexion = null;
+		costo = 1500;
+		consumo = 0;
+		conexion = null;
 	}
 
+	public void actuar(ArrayList<Visitable> objetivos){
+		for (Visitable v : objetivos){
+			v.aceptar(this);
+		}
+	}
+	
 	// TODO Revisar si sirve el metodo
 	public void conectarTuberia(Conector unaTuberia) {
-		this.conexion = unaTuberia;
+		conexion = unaTuberia;
 	}
 
 	public Conector getConexion() {
-		return this.conexion;
+		return conexion;
+	}
+
+	@Override
+	public void visitar(UnidadResidencial unaUnidadResidencial) {
+		unaUnidadResidencial.repararse();		
+	}
+
+	@Override
+	public void visitar(UnidadComercial unaUnidadComercial) {
+		unaUnidadComercial.repararse();		
+	}
+
+	@Override
+	public void visitar(UnidadIndustrial unaUnidadIndustrial) {
+		unaUnidadIndustrial.repararse();		
+	}
+
+	@Override
+	public void visitar(UnidadEnergetica unaUnidadEnergetica) {
+		unaUnidadEnergetica.repararse();		
+	}
+
+	@Override
+	public void visitar(LineaTension unaLineaTension) {
+		unaLineaTension.repararse();		
+	}
+
+	@Override
+	public void visitar(Ruta unaRuta) {
+		unaRuta.repararse();
 	}
 
 }
