275e25272bfc21a3f80890c7ce3add1a02b67ca1
Carlos <carlosmbollero@gmail.com>
2014-11-28 22:07:08 -0300

    Corrección de nombre

--- test/algo3/algocity/model/GodzillaTest.java
+++ test/algo3/algocity/model/GodzillaTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class GonzillaTest {
+public class GodzillaTest {
 
 	@Test
 	public void testSePuedeDaniarUnaUnidadResidencial() {
