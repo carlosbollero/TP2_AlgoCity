63a856cf8b2a2691ca20707f7a0f58d8b7ed0227
Carlos <carlosmbollero@gmail.com>
2014-11-30 01:30:18 -0300

    Cambio en el constructor

--- src/algo3/algocity/model/CentralEolica.java
+++ src/algo3/algocity/model/CentralEolica.java
@@ -1,11 +1,24 @@
 package algo3.algocity.model;
 
+import java.util.ArrayList;
+
+import algo3.algocity.model.excepciones.NoSeCumplenLosRequisitosException;
+
 public class CentralEolica extends UnidadEnergetica {
 
-	public CentralEolica() {
-		this.costo = 1000;
-		this.capacidad = 100;
-		this.radioDeInfluencia = 4;
+	public CentralEolica(ArrayList<Mapa> mapas, int x, int y) throws NoSeCumplenLosRequisitosException {
+		
+		requisitos = new RequisitoTierra();
+		
+		if (requisitos.evaluar(mapas, x, y)){
+			this.costo = 1000;
+			this.capacidad = 100;
+			this.radioDeInfluencia = 4;
+		}else{
+			throw new NoSeCumplenLosRequisitosException();
+		}
+		
+		
 	}
 
 }
