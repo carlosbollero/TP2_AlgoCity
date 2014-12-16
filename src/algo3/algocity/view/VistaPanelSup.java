fac295f61ea252036094f7889356b3b7300fe1e3
Carlos <carlosmbollero@gmail.com>
2014-12-16 01:57:43 -0300

    Desde Menu-->Salir se puede terminar la ejecución del juego. Agregado el menu Ayuda-->Acerca (a futuro dará info de los alumnos).

--- src/algo3/algocity/view/VistaPanelSup.java
+++ src/algo3/algocity/view/VistaPanelSup.java
@@ -7,7 +7,10 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
+import com.sun.xml.internal.bind.v2.model.core.MaybeElement;
+
 import algo3.algocity.controller.AccionMouseGuardarJuego;
+import algo3.algocity.controller.AccionMouseSalirJuego;
 import algo3.algocity.controller.AccionMouseVistaSubterranea;
 import algo3.algocity.controller.AccionMouseVistaSuperficial;
 import algo3.algocity.model.Juego;
@@ -20,20 +23,23 @@ public class VistaPanelSup extends JMenuBar {
 	VistaMapa vistaMapa;
 
 	JMenu m_menu;
-	JMenu m_vista;
-
 	JMenuItem i_guardar;
 	JMenuItem i_salir;
+	
+	JMenu m_vista;
 	JMenuItem i_superficial;
 	JMenuItem i_subterranea;
+	
+	JMenu m_ayuda;
+	JMenuItem i_acerca;
 
 	public VistaPanelSup(VistaMapa vista, Juego juego) {
 		this.juego = juego;
 		vistaMapa = vista;
 		setMenuArchivo();
-		add(m_menu);
 		setMenuVista();
-		add(m_vista);
+		setMenuAyuda();
+		
 	}
 
 	private void setMenuArchivo() {
@@ -41,9 +47,12 @@ public class VistaPanelSup extends JMenuBar {
 		i_guardar = new JMenuItem("Guardar");
 		i_guardar.addActionListener(new AccionMouseGuardarJuego(juego));
 		i_salir = new JMenuItem("Salir");
+		i_salir.addActionListener(new AccionMouseSalirJuego());
 		m_menu.add(i_guardar);
 		m_menu.addSeparator();
 		m_menu.add(i_salir);
+
+		add(m_menu);
 	}
 
 	private void setMenuVista() {
@@ -54,6 +63,17 @@ public class VistaPanelSup extends JMenuBar {
 		i_subterranea.addActionListener(new AccionMouseVistaSubterranea(vistaMapa));
 		m_vista.add(i_superficial);
 		m_vista.add(i_subterranea);
+		
+		add(m_vista);
+	}
+	
+	private void setMenuAyuda() {
+		m_ayuda = new JMenu("Ayuda");
+		i_acerca = new JMenuItem("Acerca");
+//		i_acerca.addActionListener(new AccionMouseAcerca());
+		m_ayuda.add(i_acerca);
+		
+		add(m_ayuda);
 	}
 
 }
