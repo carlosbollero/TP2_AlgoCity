03bf86e5daa0274f16e4715591e3fc0ffb7d1571
Carlos <carlosmbollero@gmail.com>
2014-12-18 01:08:31 -0300

    El panel de info es observador de turno y poblacion

--- src/algo3/algocity/view/VistaPanelInfo.java
+++ src/algo3/algocity/view/VistaPanelInfo.java
@@ -1,37 +1,62 @@
 package algo3.algocity.view;
 
-import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.util.Observable;
+import java.util.Observer;
 
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
 
 import algo3.algocity.model.Juego;
 
-public class VistaPanelInfo extends JPanel {
+public class VistaPanelInfo extends JPanel implements Observer {
 
 	private static final long serialVersionUID = -7918349684154728725L;
 
-	JTextPane texto;
+	JTextPane textoPoblacion;
+	JTextPane textoOcupacion;
+	JTextPane textoTurno;
 	Juego juego;
 
 	public VistaPanelInfo(Juego juego) {
 		this.juego = juego;
-		setLayout(new BorderLayout());
+		this.juego.turno().addObserver(this);
+		this.juego.poblacion().addObserver(this);
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		setPreferredSize(new Dimension(200, 250));
 		setBorder(BorderFactory.createTitledBorder("Info"));
 		
-		texto = new JTextPane();
-		texto.setBackground(null);
-		texto.setEditable(false);
+		textoPoblacion = new JTextPane();
+		textoPoblacion.setBackground(null);
+		textoPoblacion.setEditable(false);
+		textoPoblacion.setBorder(BorderFactory.createEtchedBorder());
+		textoPoblacion.setText("Poblacion  " + juego.poblacion().getCantidad());
 		
-		add(texto, BorderLayout.WEST);
-		agregarTexto("Poblacion  " + juego.poblacion().getCantidad());
+		textoOcupacion = new JTextPane();
+		textoOcupacion.setBackground(null);
+		textoOcupacion.setEditable(false);
+		textoOcupacion.setBorder(BorderFactory.createEtchedBorder());
+		textoOcupacion.setText("Ocupacion  " + juego.poblacion().getCapacidadEmpleo());
+		
+		textoTurno = new JTextPane();
+		textoTurno.setBackground(null);
+		textoTurno.setEditable(false);
+		textoTurno.setBorder(BorderFactory.createEtchedBorder());
+		textoTurno.setText("Turno  " + juego.turno().getTurno());
+		
+		add(textoTurno);
+		add(textoPoblacion);
+		add(textoOcupacion);
 	}
 
-	public void agregarTexto(String texto) {
-		this.texto.setText(texto);
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		textoTurno.setText("Turno  " + juego.turno().getTurno());
+		textoPoblacion.setText("Poblacion  " + juego.poblacion().getCantidad());
+		textoOcupacion.setText("Ocupacion  " + juego.poblacion().getCapacidadEmpleo());
+		
 	}
 
 }
