7d8b3931ba8af0a5f776d5c1d86f33e4ab69d282
Carlos <carlosmbollero@gmail.com>
2014-12-22 22:11:42 -0300

    formateado a estandar java

--- test/algo3/algocity/model/RegistroUsuariosTest.java
+++ test/algo3/algocity/model/RegistroUsuariosTest.java
@@ -10,64 +10,52 @@ import algo3.algocity.model.excepciones.NoSeEncontroElFicheroException;
 
 public class RegistroUsuariosTest {
 
-	
 	@Test
-	public void testSePuedenLeerLosNombresDeUsuariosPreviamenteGuardados() throws NoSeEncontroElFicheroException{
-		
+	public void testSePuedenLeerLosNombresDeUsuariosPreviamenteGuardados()
+			throws NoSeEncontroElFicheroException {
+
 		RegistroUsuarios ru = new RegistroUsuarios();
-		
+
 		ArrayList<String> nombresUsers = ru.nombresUsuarios();
-		
-		assertFalse(nombresUsers.isEmpty());		
+
+		assertFalse(nombresUsers.isEmpty());
 	}
-	
-	
-	
-	
-	
-	
-	
-	//COMENTADO POR EL MOMENTO
+
+	// COMENTADO POR EL MOMENTO
 	/*
-	@Test
-	public void testSePuedeCrearUnNuevoUsuario() {
-
-		ControladorUsuarios controlador = new ControladorUsuarios();
-		controlador.addUsuario("Pedro");
-
-		assertTrue(controlador.existeNombreUsuario("Pedro"));
-		assertTrue(controlador.existeUsuario(controlador.getUsuario("Pedro")));
-	}
-
-	@Test
-	public void testNoSePuedenCrearDosUsuariosConMismoNombre() {
-
-		ControladorUsuarios controlador = new ControladorUsuarios();
-		controlador.addUsuario("Juan");
-
-		assertTrue(controlador.existeUsuario(controlador.getUsuario("Juan")));
-
-		assertTrue(controlador.existeNombreUsuario("Juan"));
-		assertFalse(controlador.addUsuario("Juan"));
-
-	}
-
-	@Test
-	public void testObtenerUsuariosYaExistentes() {
-
-		ControladorUsuarios controlador = new ControladorUsuarios();
-		controlador.addUsuario("Jose");
-		controlador.addUsuario("Martin");
-		controlador.addUsuario("Fernando");
-
-		assertTrue(controlador.getUsuario("Jose") == controlador
-				.buscarUsuario("Jose"));
-		assertTrue(controlador.getUsuario("Martin") == controlador
-				.buscarUsuario("Martin"));
-		assertTrue(controlador.getUsuario("Fernando") == controlador
-				.buscarUsuario("Fernando"));
-
-	}
-	*/
+	 * @Test public void testSePuedeCrearUnNuevoUsuario() {
+	 * 
+	 * ControladorUsuarios controlador = new ControladorUsuarios();
+	 * controlador.addUsuario("Pedro");
+	 * 
+	 * assertTrue(controlador.existeNombreUsuario("Pedro"));
+	 * assertTrue(controlador.existeUsuario(controlador.getUsuario("Pedro"))); }
+	 * 
+	 * @Test public void testNoSePuedenCrearDosUsuariosConMismoNombre() {
+	 * 
+	 * ControladorUsuarios controlador = new ControladorUsuarios();
+	 * controlador.addUsuario("Juan");
+	 * 
+	 * assertTrue(controlador.existeUsuario(controlador.getUsuario("Juan")));
+	 * 
+	 * assertTrue(controlador.existeNombreUsuario("Juan"));
+	 * assertFalse(controlador.addUsuario("Juan"));
+	 * 
+	 * }
+	 * 
+	 * @Test public void testObtenerUsuariosYaExistentes() {
+	 * 
+	 * ControladorUsuarios controlador = new ControladorUsuarios();
+	 * controlador.addUsuario("Jose"); controlador.addUsuario("Martin");
+	 * controlador.addUsuario("Fernando");
+	 * 
+	 * assertTrue(controlador.getUsuario("Jose") == controlador
+	 * .buscarUsuario("Jose")); assertTrue(controlador.getUsuario("Martin") ==
+	 * controlador .buscarUsuario("Martin"));
+	 * assertTrue(controlador.getUsuario("Fernando") == controlador
+	 * .buscarUsuario("Fernando"));
+	 * 
+	 * }
+	 */
 
 }
